12c6fc6e4304a4f3091c93a339f8d5700f936049
Hasini <mekalahasinikrishna7@gmail.com>
2026-05-19 18:19:48 +0530

    Intial project setup

---
+++ task.h
@@ -0,0 +1,16 @@
+#ifndef TASK_H
+#define TASK_H
+
+#include <iostream>
+using namespace std;
+
+struct Task
+{
+    string topic;
+    string subject;
+    string deadline;
+    int priority;
+    string status;
+};
+
+#endif
